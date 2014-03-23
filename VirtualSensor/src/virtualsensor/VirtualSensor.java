b3b1a31e56848cb6b7858891fe2a3d962433d9f8
cody <cody@ydoc>
2014-03-23 10:57:05 -0700

    add virtual sensor view component

---
+++ VirtualSensor/src/virtualsensor/VirtualSensor.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package virtualsensor;
+
+/**
+ *
+ * @author cody
+ */
+public class VirtualSensor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new View().setVisible(true);
+    }
+    
+}
